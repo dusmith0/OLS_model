f4052a37003e68195d51cce77b93ff5a820e8f7d
Dustin Smith <dusmith0@tamu.edu>
2023-09-04 14:10:30 -0500

    Created an initial formula for generating the least squares estimate. I also added some comments and spaces to make it easier for me to read.

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -2,31 +2,37 @@
 # where epsilon is normal zero with variance sigma^2 independent across samples. 
 # Seed should be set at the beginning of the function
 
+
+####Function One
 # X - design matrix, rows are n samples
 # beta - given parameter vector (could be a vector or a matrix with 1 column)
 # sigma - standard deviation of the noise, scalar
 # seed  - starting seed value, integer
 
-
 generateY <- function(X, beta, sigma, seed = 5832652){
-  #Set seed and generate Y following linear model
+  #Set seed and generate Y following linear model, note to self: default seed is 5832562
   set.seed(seed)
   Y <- X%*%beta + rnorm(nrow(X), mean = 0, sd = sigma)
   # Return Y
   return(Y)
 }
 
+
+####Function Two
 # Calculate beta_LS - least-squares solution, do not use lm function
 # You can assume that X has full rank, so X'X inverse exists
 # X - design matrix, rows are n samples
 # Y - response vector (could be a vector or a matrix with 1 column)
+
 calculateBeta <- function(X, Y){
   # Calculate beta_LS
-  
+  beta_LS <- solve(X%*%t(X))%*%t(X)*Y
   # Return beta
   return(beta_LS)
 }
 
+
+####Funciton Three
 # Calculate estimation error, defined as ||beta - beta_LS||_2^2
 # beta - true coefficient vector (could be a vector or a matrix with 1 column)
 # beta_LS - vector estimated by LS (could be a vector or a matrix with 1 column)
