ec5bff2d0ba3a9861ab210e31abf60967785ca11
Dustin Smith <dusmith0@tamu.edu>
2023-09-04 13:58:44 -0500

    Added an initial test of the first funciton. Set x, beta, and sigma values, and ran it through the function with a plot and lm summary. I found very odd estimate values.

--- Tests.R
+++ Tests.R
@@ -2,3 +2,19 @@
 
 # Source the functions
 source("FunctionsLM.R")
+
+# A random set of small data for testing the function created in FunctionsLM.R
+# to allow for testing if the functions work. 
+
+x <- matrix(c(rep(1,5),1:5),nrow=5)
+beta <- c(2,3) 
+sigma <- 3
+
+
+#First test
+y_values <- generateY(x,beta,sigma)
+plot(x[ ,2],y_values)
+summary(lm(y_values~x[ ,1]))
+
+
+
