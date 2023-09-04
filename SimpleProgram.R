bf14369fafe468c2ffeb4caf4008cefa369ba3d1
Dustin Smith <dusmith0@tamu.edu>
2023-09-04 17:27:55 -0500

    I changed the seed to 678910, as required, and adjusted the comments.

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -27,20 +27,20 @@ calculateEstimationError(beta,beta_LS)
 n = 200 # sample size for testing data
 Xtest = matrix(rnorm(n * p), n, p) # n by p matrix of covariates
 # [ToDo] Use generateY function to generate Ytest for testing data with seed = 678910
-Y <- generateY(Xtest, beta, sigma)
+Y <- generateY(Xtest, beta, sigma, seed=678910)
 beta_LS <- calculateBeta(Xtest,Y)
 
 # [ToDo] Use calculatePredictionError to asses the prediction error on Ytest. 
   #Report the error in the comments.
 prediction_error <- calculatePredictionError(Y,Xtest,beta_LS)
-prediction_error #this produced an error of 26.23676
+prediction_error #this produced an error of 26.71015
 
 # [ToDo] Use calculatePredictionError to asses the prediction error on Ytest based only on the first covariate.
 # Report the error in the comments.
 # Hint: to avoid error of non-conformable arguments, use Xtest[, 1, drop = FALSE]
 single_X <- Xtest[ , 1, drop = FALSE]
-Y <- generateY(single_X, beta[1], sigma)
+Y <- generateY(single_X, beta[1], sigma, seed=678910)
 beta_LS <- calculateBeta(single_X,Y)
 prediction_error <- calculatePredictionError(Y,single_X,beta_LS[1])
-prediction_error #this produced an error of 27.02068
+prediction_error #this produced an error of 27.28538
 
